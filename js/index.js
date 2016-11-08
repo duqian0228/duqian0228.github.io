aa7de6a6d423e02d60e83b2dabc3bf2569c8cd5b
duqian0228 <940856462@qq.com>
2016-11-08 09:13:04 +0800

    two

--- js/index.js
+++ js/index.js
@@ -23,15 +23,15 @@ $(function(){
         $(".team .works").removeClass("active");
         var index=$(this).index();
         $(".team .works").eq(index).addClass("active");
-    })
+    });
     $(".team .container .work").height($(".team .container .work .works").height());
     $(".team .container .works .works-box a").on("mousemove mouseover",function(){
         $(".team .container .works .works-box a .zhezhao").removeClass("active");
         $(this).find(".zhezhao").toggleClass("active");
-    })
+    });
     $(".team .container .skills .skill").on("mousemove",function(){
         $(this).addClass("active").find(".skill-zhezhao").addClass("active");
-    })
+    });
     $(".team .container .skills .skill").on("mouseout",function(){
         $(this).removeClass("active")
             .find(".skill-zhezhao")
@@ -40,5 +40,4 @@ $(function(){
                 $(this).removeClass("active").dequeue();
             });
     })
-
 });
